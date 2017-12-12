8a4425dceb41a50cbbd7bedb17d7c61856cc6ef4
AgusJudistira <34447974+AgusJudistira@users.noreply.github.com>
2017-12-12 18:23:11 +0100

    Invoeren van nieuwe adressen toegevoegd
    Ook het vertonen van adressen responsief gemaakt.

--- Adresboek.js
+++ Adresboek.js
@@ -1,15 +1,3 @@
-/*    this.voornaam="voornaam";
-    this.achternaam="achternaam";
-    this.voorvoegsel="voorvoegsel";
-    this.telefoonnummer="010-1234567";
-    this.mobielnummer="06-12345678";
-    this.email=email="mail@provider.nl";
-    this.straat=straat="straatnaam";
-    this.huisnummer="huisnummer";
-    this.postcode="postcode";
-    this.plaats="plaats";
-    this.land="land";
-*/
 /* eslint-env browser */ //dit is echt nodig om onterechte Brackets foutmeldingen te onderdrukken.
 /* global document */ // een andere variant om onterechte Brackets foutmeldingen te onderdrukken
    
@@ -40,68 +28,103 @@ function AdresRecord() {
         this.land=land;    
     }
     
-    this.recordToevoegen=function() {
-        alert("in recordToevoegen()");
-        
-        formulierTonen();
+    this.formulierTonen=function() {
+        document.getElementById("mutatieFormulier").style.display = "flex";
+    }
+    
+    this.formulierVerbergen=function() {
+        document.getElementById("mutatieFormulier").style.display = "none";  
     }
     
     this.inHTMLzetten=function() {
-        var naam = document.forms["Formulier"]["voornaam"].value+" "+document.forms["Formulier"]["voorvoegsel"].value+" "+document.forms["Formulier"]["achternaam"].value;
-        
+        // <span> nodes zijn onder <p> nodes ingevoegd om elk waarde addesseerbaar te maken.
+                
         var div_node=document.createElement("div");
         div_node.className="eenAdres";
         var p_node=document.createElement("p");
         div_node.appendChild(p_node);
-        var textnode=document.createTextNode(naam);
-        p_node.appendChild(textnode);
+        
+        var span_node = document.createElement("span");
+        var textnode = document.createTextNode(document.forms["Formulier"]["voornaam"].value+" ");
+        span_node.appendChild(textnode);
+        p_node.appendChild(span_node);
+        
+        span_node = document.createElement("span");
+        textnode = document.createTextNode(document.forms["Formulier"]["voorvoegsel"].value+" ");
+        span_node.appendChild(textnode);
+        p_node.appendChild(span_node);
+
+        span_node = document.createElement("span");
+        textnode = document.createTextNode(document.forms["Formulier"]["achternaam"].value);
+        span_node.appendChild(textnode);        
+        p_node.appendChild(span_node);
         
         var telefoon = document.forms["Formulier"]["telefoonnummer"].value;
         p_node = document.createElement("p");
         div_node.appendChild(p_node);
+        span_node = document.createElement("span");
+        p_node.appendChild(span_node);
         textnode = document.createTextNode(telefoon);
-        p_node.appendChild(textnode);
+        span_node.appendChild(textnode);
         
         var mobiel = document.forms["Formulier"]["mobielnummer"].value;
         p_node = document.createElement("p");
         div_node.appendChild(p_node);
+        span_node = document.createElement("span");
+        p_node.appendChild(span_node);
         textnode = document.createTextNode(mobiel);
-        p_node.appendChild(textnode);
+        span_node.appendChild(textnode);
         
         var email = document.forms["Formulier"]["email"].value;
         p_node = document.createElement("p");
         div_node.appendChild(p_node);
+        span_node = document.createElement("span");
+        p_node.appendChild(span_node);
         textnode = document.createTextNode(email);
-        p_node.appendChild(textnode);
+        span_node.appendChild(textnode);
         
         var website = document.forms["Formulier"]["website"].value;
         p_node = document.createElement("p");
         div_node.appendChild(p_node);
+        span_node = document.createElement("span");
+        p_node.appendChild(span_node);
         textnode = document.createTextNode(website);
-        p_node.appendChild(textnode);
+        span_node.appendChild(textnode);
         
         var straatLocatie = document.forms["Formulier"]["straatlocatie"].value;
         p_node = document.createElement("p");
         div_node.appendChild(p_node);
+        span_node = document.createElement("span");
+        p_node.appendChild(span_node);
         textnode = document.createTextNode(straatLocatie);
-        p_node.appendChild(textnode);
-        
-        var postcode = document.forms["Formulier"]["postcode"].value;
-        var plaats = document.forms["Formulier"]["plaats"].value;
+        span_node.appendChild(textnode);
+                
         p_node = document.createElement("p");
         div_node.appendChild(p_node);
-        textnode = document.createTextNode(postcode+" "+plaats);
-        p_node.appendChild(textnode);
+        
+        span_node = document.createElement("span");
+        textnode = document.createTextNode(document.forms["Formulier"]["postcode"].value);
+        span_node.appendChild(textnode);
+        p_node.appendChild(span_node);
+        
+        span_node = document.createElement("span");
+        textnode = document.createTextNode(document.forms["Formulier"]["plaats"].value);
+        span_node.appendChild(textnode);
+        p_node.appendChild(span_node);
         
         var land = document.forms["Formulier"]["land"].value;
         p_node = document.createElement("p");
         div_node.appendChild(p_node);
+        span_node = document.createElement("span");
+        p_node.appendChild(span_node);
         textnode = document.createTextNode(land);
-        p_node.appendChild(textnode);
+        span_node.appendChild(textnode);
         
         document.getElementById("adressenContainer").appendChild(div_node);
         
-        formulierVerbergen();    
+        this.formulierVerbergen();
+        var invulFormulier = document.getElementsByName("Formulier");
+        invulFormulier[0].reset(); // Formulier moet schoongemaakt worden. Klaar voor volgende invoer.
     }
     
     this.recordMuteren=function() {
@@ -122,7 +145,7 @@ function AdresRecord() {
 
 function startAdresboek() {
     initialiseren();
-    formulierVerbergen();
+    adres.formulierVerbergen();
 }
 
 function initialiseren() {
@@ -151,7 +174,7 @@ function initialiseren() {
     elem[0].innerHTML += adres.land+"<br />";
     alert("is geinitialiseerd");
 }
-
+/*
 
 function formulierTonen() {
    
@@ -161,7 +184,7 @@ function formulierTonen() {
 function formulierVerbergen() {
     
     document.getElementById("mutatieFormulier").style.display = "none";
-}
+}*/
 
 var adres;
 window.onload = startAdresboek();
