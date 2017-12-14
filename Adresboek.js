3766ca48835905285405d4244efdae9b0dd45348
AgusJudistira <34447974+AgusJudistira@users.noreply.github.com>
2017-12-14 13:44:11 +0100

    Add files via upload

--- Adresboek.js
+++ Adresboek.js
@@ -2,6 +2,7 @@
 /* global document */ // een andere variant om onterechte Brackets foutmeldingen te onderdrukken
    
 function AdresRecord() {
+    //vulRecord is alleen maar om te initialiseren in de testfase.
     this.vulRecord=function(voornaam,
                             achternaam,
                             voorvoegsel,
@@ -9,8 +10,7 @@ function AdresRecord() {
                             mobielnummer,
                             email,
                             website,
-                            straatlocatie,
-                            huisnummer,
+                            straatlocatie,                            
                             postcode,
                             plaats,
                             land) {
@@ -22,7 +22,6 @@ function AdresRecord() {
         this.email=email;
         this.website=website;
         this.straatlocatie=straatlocatie;
-        this.huisnummer=huisnummer;
         this.postcode=postcode;
         this.plaats=plaats;
         this.land=land;    
@@ -34,10 +33,21 @@ function AdresRecord() {
     
     this.formulierVerbergen=function() {
         document.getElementById("mutatieFormulier").style.display = "none";  
-    }
+    }        
     
     this.inHTMLzetten=function() {
-        // <span> nodes zijn onder <p> nodes ingevoegd om elk waarde addesseerbaar te maken.
+        // <span> nodes zijn onder <p> nodes ingevoegd om elk veld addesseerbaar te maken. <p> nodes zijn puur voor de opmaak.
+        /*this.vulRecord(document.forms["Formulier"]["voornaam"].value,
+                       document.forms["Formulier"]["achternaam"].value,
+                       document.forms["Formulier"]["voorvoegsel"].value,
+                       document.forms["Formulier"]["telefoonnummer"].value,
+                       document.forms["Formulier"]["mobielnummer"].value),
+                       document.forms["Formulier"]["email"].value,
+                       document.forms["Formulier"]["website"].value,
+                       document.forms["Formulier"]["straatlocatie"].value,
+                       document.forms["Formulier"]["postcode"].value,
+                       document.forms["Formulier"]["plaats"].value,
+                       document.forms["Formulier"]["land"].value);*/
                 
         var div_node=document.createElement("div");
         div_node.className="eenAdres";
@@ -103,7 +113,7 @@ function AdresRecord() {
         div_node.appendChild(p_node);
         
         span_node = document.createElement("span");
-        textnode = document.createTextNode(document.forms["Formulier"]["postcode"].value);
+        textnode = document.createTextNode(document.forms["Formulier"]["postcode"].value + " ");
         span_node.appendChild(textnode);
         p_node.appendChild(span_node);
         
@@ -120,8 +130,19 @@ function AdresRecord() {
         textnode = document.createTextNode(land);
         span_node.appendChild(textnode);
         
-        document.getElementById("adressenContainer").appendChild(div_node);
+        var elem=document.getElementsByClassName("eenAdres");
+        if (elem[0].innerHTML != eersteHTMLAdres) { // als er al eerder adressen zijn ingevoerd
+            // nieuwe adres toevoegen
+            document.getElementById("adressenContainer").appendChild(div_node);
+        }
+        else {
+            // bestaande melding vervangen door het nieuwe adres
+            
+            //elem[0] = div_node;
+            elem[0].parentElement.replaceChild(div_node,elem[0]);
+        }
         
+        this.bewaarAdressen();
         this.formulierVerbergen();
         var invulFormulier = document.getElementsByName("Formulier");
         invulFormulier[0].reset(); // Formulier moet schoongemaakt worden. Klaar voor volgende invoer.
@@ -140,51 +161,89 @@ function AdresRecord() {
             })(i);
         }
     }
-
+    
+    this.bewaarAdressen = function() {
+        //var adressen = document.getElementsByClassName("eenAdres");
+        var adressen = document.getElementById("adressenContainer").innerHTML;
+        
+        localStorage.setItem("alleAdressen", JSON.stringify(adressen)); 
+        alert(JSON.stringify(adressen));
+    }
+    
+    this.laadAdressen = function() {
+        var bewaardeAdressen = JSON.parse(localStorage.getItem("alleAdressen"));
+        console.log(bewaardeAdressen);
+        /*for (var i=0;i<bewaardeAdressen.length;i++) {
+            console.log(bewaardeAdressen[i]);
+        }*/
+        
+        document.getElementById("adressenContainer").innerHTML = bewaardeAdressen;
+        alert("Adressen geladen");
+    }
+    
 }
 
 function startAdresboek() {
     initialiseren();
     adres.formulierVerbergen();
+    
 }
 
 function initialiseren() {
-    adres = new AdresRecord();
-    adres.vulRecord("Voornaam",
-                   "Achternaam",
-                   "voorvoegsel",
-                   "123-1234567",
-                   "06-12345678",
-                   "iemand@provider.nl",
-                   "www.website.nl",
-                   "Grote Straat",
-                   "99c",
-                   "9999 XZ",
-                   "BigCity",
-                   "Nederland");
     
-    var elem = document.getElementsByClassName("eenAdres");
-    elem[0].innerHTML = adres.voornaam+" "+adres.voorvoegsel+" "+adres.achternaam+"<br />";
-    elem[0].innerHTML += adres.telefoonnummer+"<br />";
-    elem[0].innerHTML += adres.mobielnummer+"<br />";
-    elem[0].innerHTML += adres.email+"<br />";
-    elem[0].innerHTML += adres.website+"<br />";
-    elem[0].innerHTML += adres.straat+" "+adres.huisnummer+"<br />";
-    elem[0].innerHTML += adres.postcode+" "+adres.plaats+"<br />";
-    elem[0].innerHTML += adres.land+"<br />";
-    alert("is geinitialiseerd");
-}
-/*
-
-function formulierTonen() {
-   
-    document.getElementById("mutatieFormulier").style.display = "flex";
-}
-
-function formulierVerbergen() {
+    eersteHTMLAdres = document.getElementsByClassName("eenAdres")[0].innerHTML;
     
-    document.getElementById("mutatieFormulier").style.display = "none";
-}*/
+    if (typeof(Storage) !== "undefined") { // als de browser localStorage ondersteunt
+        // Laden met localStorage
+        
+        adres = new AdresRecord();
+        
+        var bewaardeAdressen = JSON.parse(localStorage.getItem("alleAdressen"));
+        //alert("localStorage.length: "+localStorage.length);
+        
+        if (bewaardeAdressen === null) { // als er geen adressen zijn opgeslagen
+            alert("Er zijn geen opgeslagen adressen");
+            var elem = document.getElementsByClassName("eenAdres");
+            elem[0].innerHTML="Er zijn nog geen adressen ingevoerd.";
+            
+            /* adres.vulRecord("Voornaam",
+                           "Achternaam",
+                           "voorvoegsel",
+                           "123-1234567",
+                           "06-12345678",
+                           "iemand@provider.nl",
+                           "www.website.nl",
+                           "Grote Straat",
+                           "99c",
+                           "9999 XZ",
+                           "BigCity",
+                           "Nederland");
 
+            var elem = document.getElementsByClassName("eenAdres");
+            elem[0].innerHTML = adres.voornaam+" "+adres.voorvoegsel+" "+adres.achternaam+"<br />";
+            elem[0].innerHTML += adres.telefoonnummer+"<br />";
+            elem[0].innerHTML += adres.mobielnummer+"<br />";
+            elem[0].innerHTML += adres.email+"<br />";
+            elem[0].innerHTML += adres.website+"<br />";
+            elem[0].innerHTML += adres.straat+" "+adres.huisnummer+"<br />";
+            elem[0].innerHTML += adres.postcode+" "+adres.plaats+"<br />";
+            elem[0].innerHTML += adres.land+"<br />"; */
+        }
+        else {
+            // adressenContainer vullen met opslag
+            adres.laadAdressen();            
+        }
+        alert("is geinitialiseerd");
+    } else {
+        // Sorry! Geen opslagmogelijkheid.
+        alert("Gegevens kunnen niet opgeslagen worden op deze browser!\n"
+            +"Adresboek kan hier niet op werken.");
+        
+    }
+ 
+}
+
+var eersteHTMLAdres;
 var adres;
 window.onload = startAdresboek();
+//window.onunload = adres.bewaarAdressen();
